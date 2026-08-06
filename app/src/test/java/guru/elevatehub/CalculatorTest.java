a979033e1ff4aa9d64be892b99cc9d03866cfd51
Ubuntu <ubuntu@ip-172-31-31-2.ec2.internal>
2026-08-06 04:43:37 +0000

    Added a multiply method to calculator and wrote a junit test for it

--- app/src/test/java/guru/elevatehub/CalculatorTest.java
+++ app/src/test/java/guru/elevatehub/CalculatorTest.java
@@ -23,4 +23,9 @@ class CalculatorTest {
         assertEquals(4, calculator.subtract(5, 1));
         assertEquals(-2, calculator.subtract(1, 3));
     }
+
+    @Test
+    void multipliesTwoNumbers() {
+        assertEquals(6, calculator.multiply(2, 3));                                                           
+        assertEquals(-20, calculator.multiply(-4, 5));                                                            }                                                  
 }
